de9cbc955054bbae9310479331af7ce4bf49a8f5
BIKOU <bikouyehkachristaloys@gmail.com>
2020-08-11 07:56:17 -0700

    Phoenix 2
    # Conflicts:
    #	app/src/main/java/com/example/luckydicerollgameoutcomestesting/MainActivity.kt

--- app/src/main/java/com/example/luckydicerollgameoutcomestesting/MainActivity.kt
+++ app/src/main/java/com/example/luckydicerollgameoutcomestesting/MainActivity.kt
@@ -4,6 +4,10 @@ fun main() {
     val myFirstDice = Dice(6)
     val rollResult = myFirstDice.roll()
     val luckyNumber = 4
+
+    when (rollResult) {
+
+    }
 }
 
 class Dice (val numSides: Int) {
